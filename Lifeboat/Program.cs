ce2b4a0bdc3cf429e6206ce86acd5f188914cdae
Bob Palmer <rbpalmer2222@gmail.com>
2014-06-28 21:29:14 -0400

    0.1.2 Update

--- Lifeboat/Program.cs
+++ Lifeboat/Program.cs
@@ -27,6 +27,11 @@ namespace LifeBoat
         [KSPField(isPersistant = true)]
         public bool isDeployed = false;
 
+        [KSPAction("Disconnect")]
+        public void DisconnectPod(KSPActionParam param)
+        {
+            DisconnectLifeboat();
+        }
         
         public Animation DeployAnimation
         {
@@ -51,57 +56,102 @@ namespace LifeBoat
             }
         }
 
+
         [KSPAction("Evacuate")]
         public void EvacuateShip(KSPActionParam param)
         {
-            if (vessel.GetCrewCount() > 0)
+            TryToEvacuate(true);
+        }
+
+        private void TryToEvacuate(bool firstTry)
+        {
+            try
             {
-                if (!isDeployed)
+                if (vessel.GetCrewCount() > 0)
                 {
-                    InflateLifeboat();
-                }
-                if (part.CrewCapacity == 1 && part.protoModuleCrew.Count == 0)
-                {
-                    ProtoCrewMember k = null;
-                    print("Evacuating!");
-                    //Remove Crewmember
-                    foreach (var p in vessel.parts)
+                    if (!isDeployed)
                     {
-                        if (p.protoModuleCrew.Count > 0)
+                        InflateLifeboat();
+                    }
+                    if (part.CrewCapacity == 1 && !part.protoModuleCrew.Any())
+                    {
+                        print("Evacuating!");
+                        var source = vessel.Parts.First(x => x != part
+                                                             && !x.Modules.Contains("LifeBoat")
+                                                             && x.protoModuleCrew.Count > 0);
+
+                        if (source != null)
                         {
-                            k = p.protoModuleCrew.First();
-                            p.RemoveCrewmember(k);
-                            k.seat = null;
-                            k.rosterStatus = ProtoCrewMember.RosterStatus.AVAILABLE;
+                            print("Attempting to evacuate...");
+                            var k = source.protoModuleCrew.First();
+                            if (k != null)
+                            {
+                                print("Evacuating " + k.name);
+                                source.RemoveCrewmember(k);
+                                k.seat = null;
+                                k.rosterStatus = ProtoCrewMember.RosterStatus.AVAILABLE;
+
+                                //Add Crewmember
+                                part.AddCrewmember(k);
+                                k.rosterStatus = ProtoCrewMember.RosterStatus.ASSIGNED;
+                                if (k.seat != null)
+                                    k.seat.SpawnCrew();
+                            }
+                            else
+                            {
+                                print("Problem evacuating crewmember...");
+                                if (firstTry)
+                                    TryToEvacuate(false);
+                            }
+                        }
+                        else
+                        {
+                            print("Could not find any crew to move");
                         }
                     }
-                    //Add Crewmember
-                    part.AddCrewmember(k);
-                    k.rosterStatus = ProtoCrewMember.RosterStatus.ASSIGNED;
-                    if(k.seat != null)
-                        k.seat.SpawnCrew();
-                    //Decouple
-                    part.decouple(200f);
                 }
             }
+            catch (Exception ex)
+            {
+                print("ERROR: " + ex.Message);
+                if (firstTry)
+                    TryToEvacuate(false);
+            }
+            
         }
+
 
         [KSPEvent(guiName = "Inflate Lifeboat", guiActive = true, externalToEVAOnly = true, guiActiveEditor = false, active = true, guiActiveUnfocused = true, unfocusedRange = 3.0f)]
         public void InflateLifeboat()
         {
-            if (!isDeployed)
+            try
             {
-                isDeployed = true;
-                DeployAnimation[deployAnimationName].speed = 1;
-                DeployAnimation.Play(deployAnimationName);
-                HatchAnimation[hatchAnimationName].speed = 1;
-                HatchAnimation.Play(hatchAnimationName); 
-                part.CrewCapacity = 1;
-                ToggleEvent("InflateLifeboat", false);
-                ToggleEvent("DeployKickstand", true);
-                AddResources();
+                if (!isDeployed)
+                {
+                    isDeployed = true;
+                    DeployAnimation[deployAnimationName].speed = 1;
+                    DeployAnimation.Play(deployAnimationName);
+                    HatchAnimation[hatchAnimationName].speed = 1;
+                    HatchAnimation.Play(hatchAnimationName);
+                    part.CrewCapacity = 1;
+                    ToggleEvent("InflateLifeboat", false);
+                    ToggleEvent("DeployKickstand", true);
+                    AddResources();
+                }
+            }
+            catch (Exception ex)
+            {
+                print("ERR in InflateLifeboat: " + ex.Message);
             }
         }
+
+        [KSPEvent(guiName = "Disconnect", guiActive = true, externalToEVAOnly = true, guiActiveEditor = false, active = true, guiActiveUnfocused = true, unfocusedRange = 3.0f)]
+        public void DisconnectLifeboat()
+        {
+            part.decouple(100f);
+            ToggleEvent("DisconnectLifeboat", false);
+        }
+
 
         [KSPEvent(guiName = "Kickstand", guiActive = true, externalToEVAOnly = true, guiActiveEditor = false, active = false, guiActiveUnfocused = true, unfocusedRange = 3.0f)]
         public void DeployKickstand()
---
+++ README.txt
@@ -0,0 +1,1 @@
+To use alternate textures, replace PodEngine.tga and PodCapsule.tga with the png's in the altTextures folder.  You will need to delete the TGAs - do NOT delete the normal maps!
