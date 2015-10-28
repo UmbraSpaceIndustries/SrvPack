f91625b9395456855574943d6896c9c05e7b28e6
drspacemonkey <drspacemonkey@gmail.com>
2015-10-28 14:28:39 -0700

    Buoyancy now scales properly

--- FloaterTools/FloaterTools/FloaterTools/FloaterModule.cs
+++ FloaterTools/FloaterTools/FloaterTools/FloaterModule.cs
@@ -10,44 +10,24 @@ namespace FloaterTools
 {
     public class FloaterModule : PartModule
     {
-
-
-
-
-        private float inflationTimeTotal = 0f;
-        private float inflationTimeLeft = 0f;
-        public float pumpSpeed = 0f;
-
         #region Fields
         [KSPField]
         public String deployAnimationName = "Deploy";
 
         [KSPField]
-        public float deployAnimationSpeed = -2f;
+        public float deployAnimationSpeed = 1f;
 
-        [KSPField]
-        public float retractAnimationSpeed = 2f;
-
-        [KSPField]
-        public float instantAnimationSpeed = 1000f;
-
-        [KSPField]
+        [KSPField(isPersistant = true)]
         public float maxBuoyancy = 50f;
-
-        [KSPField]
-        public float minBuoyancy = 0f;
 
         [KSPField]
         public float buoyancyChangeInterval = 10f;
 
-        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Inflation Status")]
-        public float inflationStatus = 0f;
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Buoyancy"), UI_FloatRange(minValue = 0f, maxValue = 100f, stepIncrement = 5f)]
+        public float buoyancyPercentageWhenDeployed = 100f;
 
-        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Buoyancy"), UI_FloatRange(minValue = 0f, maxValue = 50f, stepIncrement = 1f)]
-        public float buoyancyWhenDeployed = 25f;
-
-        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Inflation speed %"), UI_FloatRange(minValue = 10f, maxValue = 100f, stepIncrement = 10f)]
-        public float inflationSpeed = 100f;
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Pump speed"), UI_FloatRange(minValue = 10f, maxValue = 100f, stepIncrement = 10f)]
+        public float pumpPower = 100f;
 
         [KSPField(isPersistant = true)]
         public float buoyancyWhenStowed = 0f;
@@ -61,6 +41,7 @@ namespace FloaterTools
         [KSPField]
         public string FireSpitterBuoyancyModuleName = "FSbuoyancy";
 
+        private float targetInflationPercentage => isDeployed ? buoyancyPercentageWhenDeployed / 100f : 0f;
 
         #endregion
 
@@ -68,38 +49,45 @@ namespace FloaterTools
         public override void OnStart(StartState state)
         {
             DeployAnimation.wrapMode = WrapMode.ClampForever;
-            SetDeployedState();
-            if (isDeployed)
-                PlayDeployAnimation(instantAnimationSpeed);
+            DeployAnimation.Play(deployAnimationName);
+
+            DeployAnimation[deployAnimationName].normalizedTime = isDeployed ?  targetInflationPercentage : 0f;
+
+            UpdateEvents();
+
             base.OnStart(state);
         }
 
 
         public void FixedUpdate()
         {
-            inflationTimeLeft = Math.Max(0f, inflationTimeLeft - TimeWarp.fixedDeltaTime);
-
+            SetPumpSpeed();
             SyncFireSpitterBuoyancy();
         }
 
         #endregion
 
         #region Firespitter helper methods
-        private bool SyncFireSpitterBuoyancy()
+        private void SyncFireSpitterBuoyancy()
         {
-            var newBuoyancy = isDeployed ? buoyancyWhenDeployed : buoyancyWhenStowed;
-            newBuoyancy = newBuoyancy * InflationStatusMultiplier;                      //Set newBuoyancy to scale along with the animation
-            if (FireSpitterBuoyancy == newBuoyancy)
-                return true;                                                            //Firespitter is already set correctly, no need to update it
-            return part.Modules[FireSpitterBuoyancyModuleName]
-                .Fields.SetValue(FireSpitterBuoyancyFieldName, newBuoyancy);
+            var curBuoyancy = currentInflationPercentage * maxBuoyancy;
+
+            //Don't wanna needlessly change physics
+            if (FireSpitterBuoyancy != curBuoyancy)     
+                FireSpitterBuoyancy = curBuoyancy; 
         }
 
-        private float FireSpitterBuoyancy
-        {
-            get { return (float)part.Modules[FireSpitterBuoyancyModuleName]
-                    .Fields.GetValue(FireSpitterBuoyancyFieldName); }
-        } 
+        private float FireSpitterBuoyancy {
+            get
+            {
+                return (float)part.Modules[FireSpitterBuoyancyModuleName].Fields.GetValue(FireSpitterBuoyancyFieldName);
+            }
+            set
+            {
+                part.Modules[FireSpitterBuoyancyModuleName].Fields.SetValue(FireSpitterBuoyancyFieldName, value);
+            }
+        }
+            
         #endregion
 
 
@@ -108,94 +96,49 @@ namespace FloaterTools
         /// </summary>
         private void UpdateEvents()
         {
-            part.Modules[FireSpitterBuoyancyModuleName].Fields[FireSpitterBuoyancyFieldName].guiActiveEditor = false;  //Firespitter slider should be disabled everywhere
+            //Firespitter slider should be disabled everywhere
+            part.Modules[FireSpitterBuoyancyModuleName].Fields[FireSpitterBuoyancyFieldName].guiActiveEditor = false;  
             part.Modules[FireSpitterBuoyancyModuleName].Fields[FireSpitterBuoyancyFieldName].guiActive = false;
 
             Events["DeployEvent"].active = !isDeployed;
             Events["RetractEvent"].active = isDeployed;
-
-            Actions["DeployAction"].active = !isDeployed || HighLogic.LoadedSceneIsEditor;
-            Actions["RetractAction"].active = isDeployed || HighLogic.LoadedSceneIsEditor;
         }
-        
-
+                
         /// <summary>
-        /// Sets deployed state without animation
-        /// </summary>
-        private void SetDeployedState()
-        {
-            SyncFireSpitterBuoyancy();
-            UpdateEvents();
-        }
-        
-        /// <summary>
-        /// Sets a new deployed state, including animation
+        /// Sets a new deployed state
         /// </summary>
         private void SetDeployedState(bool newState)
         {
             isDeployed = newState;
-            PlayDeployAnimation();
-            SetDeployedState();
+            UpdateEvents();
         }
+
+        private float currentInflationPercentage =>
+                Mathf.Clamp01(DeployAnimation[deployAnimationName].normalizedTime);
+        
 
 
         #region Animation support methods
-        /// <summary>
-        /// Plays demployment animation at default speed
-        /// </summary>
-        /// <param name="reverse">If true, animation will play backwards</param>
-        private void PlayDeployAnimation()
+        private Animation DeployAnimation => part.FindModelAnimators(deployAnimationName)[0];
+
+        private void SetPumpSpeed()
         {
-            var playbackSpeed = isDeployed ? retractAnimationSpeed : deployAnimationSpeed;
-            playbackSpeed = playbackSpeed * (inflationSpeed / 100f);
-            PlayDeployAnimation(playbackSpeed);
+            var speed = deployAnimationSpeed * (pumpPower / 100f);
+            
+            var iTargetBuoyancy = (int)(targetInflationPercentage * 100);
+            var iCurrentBuoyancy = (int)(currentInflationPercentage * 100);
+
+            //Need to do this comparison with integers beacuse floating point can cause weirdness 
+            if (iTargetBuoyancy == iCurrentBuoyancy)
+                speed = 0f;             //current inflation level matches target. pump can be shut off
+            else if (iTargetBuoyancy > iCurrentBuoyancy)
+                speed = speed * -1;     //current inflation level is higher than target. pump needs to be reversed
+
+            DeployAnimation[deployAnimationName].speed = speed;
+
+            //normalizedTime will not stay between 0 and 1 by itself, and cause animation problems if not manually set
+            DeployAnimation[deployAnimationName].normalizedTime = currentInflationPercentage;
         }
-
-        /// <summary>
-        /// Plays deployment animation at a set speed
-        /// </summary>
-        /// <param name="reverse">If true, animation will play backwards</param>
-        private void PlayDeployAnimation(float playbackSpeed)
-        {
-            pumpSpeed = playbackSpeed;
-            DeployAnimation[deployAnimationName].speed = playbackSpeed;
-
-            var length = DeployAnimation[deployAnimationName].length;
-            var time = DeployAnimation[deployAnimationName].time;
-
-            inflationTimeTotal = Math.Abs(length / playbackSpeed);    //Needed for the ugly workaround in InflationStatusMultiplier
-            inflationTimeLeft = inflationTimeTotal;
-
-
-            DeployAnimation[deployAnimationName].normalizedTime = Mathf.Clamp01(DeployAnimation[deployAnimationName].normalizedTime);
-            DeployAnimation.Play(deployAnimationName);
-        }
-        
-        public Animation DeployAnimation
-        {
-            get
-            {
-                return part.FindModelAnimators(deployAnimationName)[0];
-            }
-        } 
-
-        /// <summary>
-        /// Gets current inflation % to allow for gradual inflation/deflation. Big scary note: relies on current animation time.
-        /// </summary>
-        public float InflationStatusMultiplier
-        {
-            get
-            {
-                inflationStatus = (inflationTimeTotal - inflationTimeLeft) / inflationTimeTotal;
-                //How much time is left on the inflation timer?
-                //Ideally, it would use Animation.IsPlaying and Animation.time, but neiher of those seem to work right when the animation is playing backwards
-                if (inflationTimeLeft > 0)
-                    return (inflationTimeTotal - inflationTimeLeft) / inflationTimeTotal;
-                else
-                    return 1f;
-            }
-        }
-
         #endregion
 
         #region Right click menu events
@@ -234,14 +177,12 @@ namespace FloaterTools
         [KSPAction("More Buoyancy")]
         public void IncreaseBuoyancyAction(KSPActionParam param)
         {
-            buoyancyWhenDeployed = Math.Min(maxBuoyancy, buoyancyWhenDeployed + buoyancyChangeInterval);
-            SyncFireSpitterBuoyancy();
+            buoyancyPercentageWhenDeployed = Math.Min(100f, buoyancyPercentageWhenDeployed + buoyancyChangeInterval);
         }
         [KSPAction("Less Buoyancy")]
         public void DecreaseBuoyancyAction(KSPActionParam param)
         {
-            buoyancyWhenDeployed = Math.Max(minBuoyancy, buoyancyWhenDeployed - buoyancyChangeInterval);
-            SyncFireSpitterBuoyancy();
+            buoyancyPercentageWhenDeployed = Math.Max(0f, buoyancyPercentageWhenDeployed - buoyancyChangeInterval);
         }
         #endregion
 
